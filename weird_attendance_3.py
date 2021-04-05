b73af588f7e1dc0300dcb865cab8d9df5b23a804
Seoyoon-Scalar <74050826+dltjdbs38@users.noreply.github.com>
2021-04-06 01:14:48 +0900

    Add files via upload
    Input
    The times(n) of calling the attendance numbers are entered in the first line.
    The n random numbers(k) are also entered in the second line by having a space between each numbers.
    
    Output
    The smallest number among the calling attendance numbers would be printed out.

---
+++ weird_attendance_3.py
@@ -0,0 +1,22 @@
+n=int(input())
+nums=input().split() # 0~9
+
+for i in range(n):
+    nums[i]=int(nums[i]) # int 처리
+# print(nums)
+
+# sol 1
+# nums.sort() # 오름차순 [19 21 38 53 62]
+# print(nums[0])
+# nums.sort(reverse=True) # 내림차순 [62 53 38 21 19]
+
+# sol 2
+# print(min(nums))
+
+# sol 3
+min=nums[0]
+for i in nums:
+  if i<min:
+      min=i
+print(min)
+
